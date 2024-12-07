5aad26419d6458123ba44ff5180f363cb44e9285
Lev Staver <levstaver@yandex.ru>
2024-12-07 19:52:49 +0500

    feat: complete 20 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -327,8 +327,16 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const emptyArr = new Array(Math.ceil(arr.length / chunkSize)).fill([]);
+  return emptyArr.map((elem) =>
+    elem.concat(
+      arr.splice(
+        0,
+        arr.length >= chunkSize ? chunkSize : chunkSize - arr.length
+      )
+    )
+  );
 }
 
 /**
