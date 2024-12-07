82c43778c6be906353a2117e664a3108d92cef46
Lev Staver <levstaver@yandex.ru>
2024-12-07 18:25:31 +0500

    feat: complete 8 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -135,8 +135,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const elemLength = arr[0].length;
+  return arr.every((elem) => elem.length === elemLength);
 }
 
 /**
