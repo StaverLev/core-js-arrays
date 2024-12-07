2d3986022315cd33d64d449760e94beabd55e983
Lev Staver <levstaver@yandex.ru>
2024-12-07 18:05:04 +0500

    feat: complete 2 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,10 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const max = arr1.length >= arr2.length ? arr1 : arr2;
+  const min = arr1.length >= arr2.length ? arr2 : arr1;
+  return max.map((elem, index) => elem + (min[index] ? min[index] : 0));
 }
 
 /**
