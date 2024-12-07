087e5e6b5c11ab680ce0c4dc3d35c2620dce4125
Lev Staver <levstaver@yandex.ru>
2024-12-07 18:19:22 +0500

    feat: complete 7 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -121,8 +121,8 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return +arr.reduce((acc, elem) => acc + elem / arr.length, 0).toFixed(2);
 }
 
 /**
